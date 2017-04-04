d423690b68ad5745d3063937b2e05f2d364dad48
Gordon Turner <gordon.turner@gmail.com>
2017-04-04 08:05:18 -0400

    Initial import

---
+++ src/main/java/com/gordonturner/reference/App.java
@@ -0,0 +1,13 @@
+package com.gordonturner.reference;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
